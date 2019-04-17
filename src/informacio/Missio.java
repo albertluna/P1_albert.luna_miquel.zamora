53014c0e9bb8640645481c0e723291eb2db1dfb9
albertluna <albert.luna@students.salle.url.edu>
2019-04-17 10:23:10 +0200

    en prusés de la missio 4

--- src/informacio/Missio.java
+++ src/informacio/Missio.java
@@ -35,6 +35,7 @@ public class Missio {
                             k = 4;
                         }
                     }
+
                     //Continua la captura si el jugador ha introduit una bola que existeix i en disposa
                     if (bolaCorrecta) {
 
